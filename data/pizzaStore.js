d8e1d3dab7c44c184c50861a2908fd9fcfe21878
Sylvain Chamberland <gentx2@gmail.com>
2020-09-09 20:30:11 +0000

    store data in RDS

---
+++ data/pizzaStore.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+
+const database = 'pizza_luvrs';
+const host = 'form-awsdev-sylvain-db.c9xto6064mzc.ca-central-1.rds.amazonaws.com';
+const username = 'postgres';
+const password = 'u3h8&excWW#^6PTq';
+
+const pgClient = new Sequelize(
+    database,
+    username,
+    password,
+    {
+        host: host,
+        dialect: 'postgres'
+    }
+);
+
+const Pizza = pgClient.define('pizza', {
+    id: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING
+    },
+    toppings: {
+        type: Sequelize.STRING
+    },
+    img: {
+        type: Sequelize.STRING
+    },
+    username: {
+        type: Sequelize.STRING
+    },
+    created: {
+        type: Sequelize.BIGINT
+    }
+});
+
+Pizza.sync().then(() => {
+    console.log('postgres connection ready');
+}).catch((reason) => {
+    console.log('erreur', reason);
+});
+
+module.exports = Pizza;
